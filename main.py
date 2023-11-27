1be7b965c7d4f8858196e9fb930bc2e5f73058d0
Ярослав <alal6091@gmail.com>
2023-11-27 17:58:43 +0700

    #

--- main.py
+++ main.py
@@ -53,3 +53,7 @@ async def get_result_note(request: Request, note_text: str):
     }
     )
 
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
